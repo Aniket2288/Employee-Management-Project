8572c1e0db04de4a74c14414271cc50bf5f308b0
Aniket2288 <112190920+Aniket2288@users.noreply.github.com>
2023-02-17 17:23:08 +0530

    first commit

---
+++ Employee/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from Employee.models import EmployeeDetails
+from .models import *
+
+# Register your models here.
+admin.site.register(EmployeeDetails)
+admin.site.register(EmployeeEducation)
+admin.site.register(EmployeeExperience)
